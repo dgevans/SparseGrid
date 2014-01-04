8a21585f4700bbb8da68ceb5d343fab06a1ee238
David Evans <dgevans42@gmail.com>
2014-01-03 17:15:02 -0800

    Initial Commit

---
+++ SparseGrid/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  SparseGrid
+//
+//  Created by David Evans on 1/3/14.
+//  Copyright (c) 2014 David Evans. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
